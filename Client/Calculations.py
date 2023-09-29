fd4600886b624526088a588768b6e2d5a27cf09a
FinnHall <finnlhall@gmail.com>
2023-09-28 19:50:08 -0600

    Made the folder Client which has calculations.py which makes the players bet. Also added a testing file for Finn.

---
+++ Client/Calculations.py
@@ -0,0 +1,13 @@
+
+def make_bet(bet, total_number_of_money):
+    all_in = False
+    amount_betting = 0
+    amount_of_money_left = 0
+    if bet >= total_number_of_money:
+        all_in = True
+        amount_betting = total_number_of_money
+    if bet <= total_number_of_money:
+        amount_betting = bet
+        amount_of_money_left = total_number_of_money - bet
+    return amount_betting, amount_of_money_left, all_in
+
