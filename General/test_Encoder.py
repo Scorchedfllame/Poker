582493cd7abdc186882f68c607424845a5e36650
CharlieCarter072 <charliecarter072@gmail.com>
2023-09-28 21:15:17 -0600

    created encoder function

--- General/test_Encoder.py
+++ General/test_Encoder.py
@@ -3,8 +3,11 @@ from Encoder import *
 
 
 class MyTestCase(unittest.TestCase):
-    def Encoder(self):
+    def test_encoder(self):
         self.assertEqual(encode(['8 of Diamonds', '9 of Clubs', '10 of Spades']), [(8, 2), (9, 1), (10, 0)])
+
+    def test_decoder(self):
+        self.assertEqual(decode([(8, 2), (9, 1), (10, 0)]), ['8 of Diamonds', '9 of Clubs', '10 of Spades'])
 
 
 if __name__ == '__main__':
