e5a472eba297c7d22cb769adc51e168447e2616b
Scorchedfllame <85722296+Scorchedfllame@users.noreply.github.com>
2023-10-06 21:18:22 -0600

    Made both 1 and 14 bind to aces for scoring

--- Initialize/Game_Innit.py
+++ Initialize/Game_Innit.py
@@ -3,7 +3,7 @@ from General.Classes import *
 
 def create_deck():
     deck = []
-    for i in range(1, 14):
+    for i in range(2, 15):
         for y in range(4):
             deck.append(Card(i, y))
     return deck
