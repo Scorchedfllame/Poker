7c1c1ab9c2adc319595aba68a8f1ceddad6e63af
Scorchedfllame <85722296+Scorchedfllame@users.noreply.github.com>
2023-10-07 14:52:35 -0600

    Moved make_bet function to new Betting.py file. Moved make_bet test too.

---
+++ General/test_Betting.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from General.Betting import *
+
+
+class TestMakeBet(TestCase):
+    def test_player(self):
+        amount_betting, amount_of_money_left, all_in = make_bet(100, 500)
+        self.assertEqual(amount_betting, 100)
+        self.assertEqual(amount_of_money_left, 400)
+        self.assertEqual(all_in, False)
+
+    def test_player1(self):
+        amount_betting, amount_of_money_left, all_in = make_bet(1000, 500)
+        self.assertEqual(amount_betting, 500)
+        self.assertEqual(amount_of_money_left, 0)
+        self.assertEqual(all_in, True)
